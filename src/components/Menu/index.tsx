ce47f698706a45f0b0e1c2bf3155d388a72c7c94
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-02-29 14:31:37 +0000

    menu item developed

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { forwardRef } from "react";
 import { View, Text } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { FontAwesome } from "@expo/vector-icons";
 
-export function Menu() {
+import { MenuProps } from "./menu";
+import { styles } from "./styles";
+import { theme } from "@/theme";
+import { MenuButton } from "@/components/MenuButton";
+
+export const Menu = forwardRef<BottomSheet, MenuProps>(({ onClose }, ref) => {
   return (
-    <View>
-      <Text>Comece</Text>
-    </View>
+    <BottomSheet
+      ref={ref}
+      index={0}
+      snapPoints={[0.01, 230]}
+      backgroundStyle={styles.container}
+      handleComponent={() => null}
+    >
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <FontAwesome
+            name="close"
+            size={24}
+            onPress={onClose}
+            color={theme.colors.white}
+          />
+          <Text style={styles.tittle}>Comece a criar agora</Text>
+        </View>
+        <View style={styles.options}>
+          <MenuButton title="Pin" icon="home" />
+          <MenuButton title="Colagem" icon="paste" />
+          <MenuButton title="Pasta" icon="folder" />
+        </View>
+      </View>
+    </BottomSheet>
   );
-}
+});
